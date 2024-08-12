d90f9bc74fcfdea120fcab395bbe920946a98a66
diegovega2001 <diegovega2000@live.com>
2024-08-12 18:49:23 -0400

    no

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,12 @@
 object Main {
   def main(args: Array[String]): Unit = {
     PrimeraAuxiliarMemera()
+    PautaAuxiliarMemera()
   }
   def PrimeraAuxiliarMemera(): Unit ={
     println("Hola memeros")
+  }
+  def PautaAuxiliarMemera(): Unit ={
+    println("me canse de ser chileno")
   }
 }
