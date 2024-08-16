8f827f924f86220e15176b5b224d649d0de314d2
Lucho <luis.munoz.caceres@ug.uchile.cl>
2024-08-15 21:48:53 -0400

    adios tonotos

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,12 @@
 object Main {
   def main(args: Array[String]): Unit = {
     PrimeraAuxiliarMemera()
+    ola()
   }
   def PrimeraAuxiliarMemera(): Unit ={
     println("Hola memeros")
   }
+def ola(): Unit = {
+  println("ola mundo")
+}
 }
